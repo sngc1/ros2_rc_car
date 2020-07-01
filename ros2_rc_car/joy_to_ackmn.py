f8e5b6fdcf9a95fbe95d69cc8a37c1deb3a4197c
sngc1 <strngch@gmail.com>
2020-07-01 15:14:07 +0000

    Bugfix

--- ros2_rc_car/joy_to_ackmn.py
+++ ros2_rc_car/joy_to_ackmn.py
@@ -14,19 +14,19 @@ class JoySubscriber(Node):
                 AckermannDrive, 'ackermann_drive', 10)
 
         self.subscription = self.create_subscription(
-            Joy,
-            'joy',
-            self.listener_callback,
-            10)
+            Joy, 'joy', self.listener_callback, 10)
+
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('Received:%s' % msg.speed)
+        # self.get_logger().info('Received joy message: %s' % msg.axes)
 
         ackmn_msg = AckermannDrive()
-        ackmn_msg.steering_angle = msg.axes[2]
-        ackmn_msg.speed = msg.axes[1]
+        ackmn_msg.steering_angle = msg.axes[0]
+        ackmn_msg.speed = msg.axes[4]
         
+        self.get_logger().info('steer: %s, speed: %s' % (ackmn_msg.steering_angle, ackmn_msg.speed))
+
         self.publisher_.publish(ackmn_msg)
         self.get_logger().info('published Ackermann msg')
 
